a212a38c6f9d18028513abf594fb885639886e5d
Rinat Ziyazetdinov <ZyazetdinovRZ@gmail.com>
2025-11-07 14:43:10 +0300

    Добавили обработку ошибок 500 и 404

--- opinions_app.py
+++ opinions_app.py
@@ -3,7 +3,7 @@ from datetime import datetime, timezone
 # Импортировать функцию для выбора случайного значения.
 from random import randrange
 
-from flask import Flask, flash, redirect, render_template, url_for
+from flask import Flask, abort, flash, redirect, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 # Новые импорты для работы с формами:
@@ -65,8 +65,9 @@ def index_view():
     quantity = Opinion.query.count()
     # Если мнений нет...
     if not quantity:
-        # ...то вернуть сообщение:
-        return 'В базе данных мнений о фильмах нет.'
+        # Если в базе пусто - при запросе к главной странице
+        # пользователь увидит ошибку 500.
+        abort(500)
     # Иначе выбрать случайное число в диапазоне от 0 до quantity...
     offset_value = randrange(quantity)
     # Извлечь все записи, пропуская первые offset_value записей
@@ -115,6 +116,27 @@ def opinion_view(id):
     opinion = Opinion.query.get_or_404(id)
     # ...и передать его в шаблон (шаблон - тот же, что и для главной страницы).
     return render_template('opinion.html', opinion=opinion)
+
+
+# Тут декорируется обработчик и указывается код нужной ошибки.
+@app.errorhandler(500)
+def internal_error(error):
+    # Ошибка 500 возникает в нештатных ситуациях на сервере.
+    # Например, провалилась валидация данных.
+    # В таких случаях можно откатить изменения, незафиксированные в БД,
+    # чтобы в базу не записалось ничего лишнего.
+    db.session.rollback()
+    # Юзеру вернётся страница, сгенерированная на основе шаблона 500.html.
+    # Этого шаблона пока нет, но сейчас вы его тоже создадите.
+    # Пользователь получит и код HTTP-ответа 500.
+    return render_template('500.html'), 500
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    # При ошибке 404 в качестве ответа вернётся страница, созданная
+    # на основе шаблона 404.html и код HTTP-ответа 404.
+    return render_template('404.html'), 404
 
 
 if __name__ == '__main__':
